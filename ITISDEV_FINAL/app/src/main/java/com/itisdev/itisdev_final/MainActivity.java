1f231735d0e90e95eba1334641048a582723ca06
hyisangie <angeline_shi@dlsu.edu.ph>
2024-11-16 16:48:50 +0800

    initial setup

---
+++ ITISDEV_FINAL/app/src/main/java/com/itisdev/itisdev_final/MainActivity.java
@@ -0,0 +1,14 @@
+package com.itisdev.itisdev_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
